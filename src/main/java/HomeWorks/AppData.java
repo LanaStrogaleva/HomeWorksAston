149c30e39eec0b7cec143934e707ab5305cea879
Svetlana Strogaleva <lana.strogaleva@gmail.com>
2023-11-19 17:16:43 +0300

    Lesson_6 Homework

---
+++ src/main/java/HomeWorks/AppData.java
@@ -0,0 +1,11 @@
+package HomeWorks;
+
+public class AppData {
+    String[] header;
+    int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+}
---
+++ src/main/java/HomeWorks/main.java
@@ -0,0 +1,49 @@
+package HomeWorks;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class main {
+    final static String File_Name = "lesson6.csv";
+
+    public static void main(String[] args) {
+        String[] header = {"Value1", "Value2", "Value3" };
+        int[][] data = {{100, 200, 123}, {300,400,500}};
+
+        AppData appData = new AppData(header, data);
+
+        save(appData);
+        load();
+    }
+    public static void save(AppData appData) {
+        try (BufferedWriter csvFile = new BufferedWriter(new FileWriter(File_Name))){
+
+            csvFile.write(String.join(";", appData.header) + "\n");
+            for (int[] app : appData.data) {
+                String[] strArray = new String[app.length];
+                for (int i = 0; i < app.length; i++) {
+                    strArray[i] = String.valueOf(app[i]);
+                }
+                csvFile.write(String.join(";",strArray) + "\n");
+            }
+        } catch (IOException e) {
+        e.printStackTrace();
+        }
+    }
+
+    public static void load() {
+        try (BufferedReader csvFile = new BufferedReader(new FileReader(File_Name))) {
+            String str;
+            while ((str = csvFile.readLine()) != null)
+                System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+}
