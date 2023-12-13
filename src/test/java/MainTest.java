c8f54698fbde44690627a18654e534ba06453e49
Svetlana Strogaleva <lana.strogaleva@gmail.com>
2023-12-13 10:11:34 +0300

    add pom.xml

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testPrintln() {
+        System.out.println("I'm a test for HelloWorld project");
+    }
+
+}
