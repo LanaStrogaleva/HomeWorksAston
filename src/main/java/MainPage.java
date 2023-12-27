5c20083af631d453577823474e4c003435182f71
Svetlana Strogaleva <lana.strogaleva@gmail.com>
2023-12-27 23:42:17 +0300

    add Lesson_15 homework

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,107 +1,101 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
 
 public class MainPage {
-    private static final String URL = "https://www.mts.by/";
+
+    private ArrayList<Good> goodList;
+    private static final String URL = "https://www.wildberries.ru/";
 
     private final WebDriver webDriver;
 
     public MainPage(WebDriver webDriver) {
-
         this.webDriver = webDriver;
+        this.goodList = new ArrayList<>();
     }
 
-    private By acceptCookieButton = By.xpath(".//button[text() = 'Принять']");
+    public ArrayList<Good> getGoodList() {
+        return goodList;
+    }
 
-    // Блок оплаты
-    private By payBlock = By.xpath((".//div[@class = 'pay__wrapper']"));
+    // список карточек товаров на главной странице
+    private By goodsList = By.className("product-card__wrapper");
 
-    // Кнопка выбора списка услуг
-    private By serviceTypeButton = By.xpath(".//button[@class = 'select__header']");
+    // Кнопка "В корзину"
+    private By cartButton = By.xpath(".//*[contains(@class, 'product-card__add-basket')]");
 
-    // список видов услуг
-    private By serviceTypesList = By.xpath(".//ul[@class = 'select__list']/li");
+    // Таблица размеров карточки (если имеется)
+    private By sizeList = By.xpath("//*[contains(@class, 'sizes-list__button')]");
 
 
-    // Плейсхолдер "Номер телефона"
-    private By phonePlaceholder = By.xpath(".//input[@placeholder = 'Номер телефона']");
+    // Корзина
+    private By headerCartButton = By.xpath(".//*[contains(@class, 'navbar-pc__icon--basket')]");
 
-    // Плейсхолдер "Сумма"
-    private By sumPlaceholder = By.xpath(".//form[@class = 'pay-form opened']//input[@placeholder = 'Сумма']");
+    // Блок карточки с наименованием товара
+    private By goodName = By.xpath(".//*[@class= 'product-card__name']");
 
-    // Плейсхолдер "E-mail для отправки чека"
-    private By emailPlaceholder = By.xpath(".//form[@class = 'pay-form opened']//input[@placeholder = 'E-mail для отправки чека']");
-
-    // Кнопка "Продолжить"
-    private By continueButton = By.xpath(".//form[@class = 'pay-form opened']//button[text() = 'Продолжить']");
-
-    // Фрейм
-    private By paymentFrame = By.className("bepaid-iframe");
-
-    // Заголовок формы оплаты
-    private By payFormHeader = By.xpath(".//div[@class= 'header__payment']/p[2]");
+    // Блок карточки с ценой товара
+    private By goodPrice = By.xpath(".//ins[@class= 'price__lower-price']");
 
     // Открыть главную страницу
     public void open() {
         webDriver.get(URL);
     }
 
-    // Нажать на кнопку "Принять" в окне "Обработка файлов cookie"
-    public void clickAcceptCookieButton() {
-        webDriver.findElement(acceptCookieButton).click();
+    public MainPage addSomeGoodsToCart(int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Actions action = new Actions(webDriver);
+            WebElement element = webDriver.findElements(goodsList).get(i);
+            action.moveToElement(element).perform();
+            webDriver.findElements(cartButton).get(i).click();
+            if (!webDriver.findElements(By.className("sizes-list__size")).isEmpty()) {
+                WebElement el = webDriver.findElements(sizeList).get(0);
+                action.moveToElement(el).perform();
+                el.click();
+            }
+            this.goodList.add(new Good(this.getGoodName(i), this.getGoodPrice(i), 1));
+        }
+        return this;
     }
 
-    // Проскроллить до блока "Онлайн пополнение без комиссии"
-    public void scrollToPayBlock() {
-        WebElement element = webDriver.findElement(payBlock);
-        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", element);
+    // Кликнуть на иконку Корзины в правом верхнем углу
+
+    public void clickHeaderCartButton() {
+        webDriver.findElement(headerCartButton).click();
     }
 
-    // Выбрать  вид усуги по индексу
-    public void clickServiceType(int index) {
-        webDriver.findElement(serviceTypeButton).click();
-        webDriver.findElements(serviceTypesList).get(index).click();
+    // Получить наименование товара из карточки товара
+    public String getGoodName(int index) {
+        return webDriver.findElements(goodName).get(index).getText().substring(2);
     }
 
-    //Получить текст плейсхолдера по id"
-    public String getPlaceholderText(String id) {
-        return webDriver.findElement(By.id(id)).getAttribute("placeholder");
+    // Получить цену товара из карточки товара
+    public int getGoodPrice(int index) {
+        String strPrice = webDriver.findElements(goodPrice).get(index).getText();
+        return Integer.parseInt(strPrice.substring(0, strPrice.length() - 2).replace(" ", ""));
     }
 
-
-    // Заполнить поле "Номер телефона"
-    public void inputPhoneField(String phone) {
-        webDriver.findElement(phonePlaceholder).sendKeys(phone);
+    // Получить общую сумму товаров
+    public int getGoodsSum() {
+        int totalSum = 0;
+        for (Good good : this.goodList
+        ) {
+            totalSum += good.getPrice();
+        }
+        return totalSum;
     }
 
-    // Заполнить поле "Сумма
-    public void inputSumField(int sum) {
-        webDriver.findElement(sumPlaceholder).sendKeys(String.valueOf(sum));
-    }
-
-    // Заполнить поле "E-mail для отправки чека"
-    public void inputEmailField(String email) {
-        webDriver.findElement(emailPlaceholder).sendKeys(email);
-    }
-
-    // Заполнить форму блока "Онлайн пополнение без комиссии"
-    public void fillPayBlockForm(TestData testData) {
-        inputPhoneField(testData.phone);
-        inputSumField(testData.sum);
-        inputEmailField(testData.email);
-    }
-
-    // Кликнуть на  кнопку "Продолжить"
-    public void clickContinueButton() {
-
-        webDriver.findElement(continueButton).click();
-    }
-
-    // Переключится на фрейм
-    public void checkoutPaymentFramePage() {
-        webDriver.switchTo().frame(webDriver.findElement(paymentFrame));
+    // Получить общee количество товаров
+    public int getTotalQuantity() {
+        int totalQuantity = 0;
+        for (Good good : this.goodList
+        ) {
+            totalQuantity += good.getQuantity();
+        }
+        return totalQuantity;
     }
 
 }
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,41 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class MainPageTest {
-    WebDriver driver;
+    private WebDriver driver;
+    private MainPage mainPage;
+    private CartPage cartPage;
 
-    // TestData testData = new TestData("297777777", 20, "test@mail.ru");
-
-    static Stream<Arguments> placeholderTestData() {
-        return Stream.of(
-                arguments(0, "connection-phone", "Номер телефона"),
-                arguments(0, "connection-sum", "Сумма"),
-                arguments(0, "connection-email", "E-mail для отправки чека"),
-                arguments(1, "internet-phone", "Номер абонента"),
-                arguments(1, "internet-sum", "Сумма"),
-                arguments(1, "internet-email", "E-mail для отправки чека"),
-                arguments(2, "score-instalment", "Номер счета на 44"),
-                arguments(2, "instalment-sum", "Сумма"),
-                arguments(2, "instalment-email", "E-mail для отправки чека"),
-                arguments(3, "score-arrears", "Номер счета на 2073"),
-                arguments(3, "arrears-sum", "Сумма"),
-                arguments(3, "arrears-email", "E-mail для отправки чека")
-        );
-    }
-
+    private final int GOODS_FOR_TEST = 5;
 
     @BeforeAll
     static void setupAll() {
@@ -46,10 +27,9 @@ public class MainPageTest {
     void setup() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        MainPage mainPage = new MainPage(driver);
+        driver.manage().window().maximize();
+        mainPage = new MainPage(driver);
         mainPage.open();
-        mainPage.clickAcceptCookieButton();
-        mainPage.scrollToPayBlock();
     }
 
     @AfterEach
@@ -57,25 +37,45 @@ public class MainPageTest {
         driver.quit();
     }
 
+    @Test
+    public void addGoodsToCartAndCheckNamePriceQuantity() throws InterruptedException {
+        mainPage
+                .addSomeGoodsToCart(GOODS_FOR_TEST)
+                .clickHeaderCartButton();
 
-    @DisplayName("Проверить текст плейсхолдеров при разных видах услуг ")
-    @ParameterizedTest
-    @MethodSource("placeholderTestData")
-    public void checkPlaceholderText(int index, String text, String expected) {
-        MainPage mainPage = new MainPage(driver);
-        mainPage.clickServiceType(index);
-        assertEquals(expected, mainPage.getPlaceholderText(text));
+        cartPage = new CartPage(driver);
+        cartPage.addGoodsList(mainPage.getGoodList().size());
+
+
+        for (int i = 0; i < mainPage.getGoodList().size(); i++) {
+            int finalI = i;
+            assertAll("Проверяем соответствие наименования, цены и количества каждого товара в корзине",
+                    () -> assertEquals(mainPage.getGoodList().get(finalI).getName(), cartPage.getGoodList().get(finalI).getName()),
+                    () -> assertEquals(mainPage.getGoodList().get(finalI).getPrice(), cartPage.getGoodList().get(finalI).getPrice()),
+                    () -> assertEquals(mainPage.getGoodList().get(finalI).getQuantity(), cartPage.getGoodList().get(finalI).getQuantity())
+            );
+        }
     }
 
-    @DisplayName("Заполнить поля и проверить работу кнопки «Продолжить»")
+    //Проверяем общую сумму
     @Test
-    public void checkContinueButton() {
-        MainPage mainPage = new MainPage(driver);
-        //mainPage.clickServiceType(0);
-        //System.out.println(mainPage.getPlaceholderText("Номер телефона"));
-        System.out.println(driver.findElement(By.xpath(".//input[@placeholder = 'Номер телефона']")).getAttribute("placeholder"));
+    public void checkTotalSum() throws InterruptedException {
+        mainPage
+                .addSomeGoodsToCart(GOODS_FOR_TEST)
+                .clickHeaderCartButton();
+        cartPage = new CartPage(driver);
+        assertEquals(mainPage.getGoodsSum(), cartPage.getTotalSum());
+    }
 
+    //Проверяем общее количество
+    @Test
+    public void checkTotalQuantity() {
+        mainPage
+                .addSomeGoodsToCart(GOODS_FOR_TEST)
+                .clickHeaderCartButton();
+
+        cartPage = new CartPage(driver);
+        assertEquals(mainPage.getTotalQuantity(), cartPage.getTotalGoodQuantity());
     }
 
 }
-
